841b5c42b532964d0b12a81d52434ce553c1a444
Kaozx001 <kaozax001@gmail.com>
2025-11-05 13:32:35 +0700

    dart practice 30/30

--- lib/dart_101.dart
+++ lib/dart_101.dart
@@ -86,11 +86,31 @@ void main() {
 
     // 26
     int day = 3;
-    switch (day) {
-      case 3: print(' Wednesday'); break;
-      default: print('Invalid Day');
-    }
-
+    switch(day) {
+        case 1:
+            print("Monday");
+            break;
+        case 2:
+            print("Tuesday");
+            break;
+        case 3:
+            print("Wednesday");
+            break;
+        case 4:
+            print("Thursday");
+            break;
+        case 5:
+            print("Friday");
+            break;
+        case 6:
+            print("Saturday");
+            break;
+        case 7:
+            print("Sunday");
+            break;
+        default:
+            print("Invalid Day");
+      }
     // 27
     for (var i = 1; i <= 10; i++) { if (i == 4) break; print(i); }
 
